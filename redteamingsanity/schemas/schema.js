805e7f3d5eb8d609410325dee3591ce1d429cd36
Me <mergorgec@gmail.com>
2020-09-17 23:41:02 +0200

    add schema types

--- redteamingsanity/schemas/schema.js
+++ redteamingsanity/schemas/schema.js
@@ -1,8 +1,8 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,6 +11,27 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
-  ])
-})
+    {
+      name: 'blog',
+      type: 'document',
+      title: 'Blog',
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+        },
+        {
+          name: 'subtitle',
+          type: 'string',
+          title: 'Subtitle',
+        },
+        {
+          name: 'slug',
+          type: 'slug',
+          title: 'Slug',
+        },
+      ],
+    },
+  ]),
+});
